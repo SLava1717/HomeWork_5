fd6b5232fd0f49b200634c61bc1a621b4e760cff
Slava K <slava.kostromin.2000@mail.ru>
2022-10-16 17:06:10 +0700

    Решил Задачу 34

--- Z_34/Program.cs
+++ Z_34/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void InputArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(99, 1000);
+}
+
+
+int ReleaseArray(int[] array)
+{
+   int sum = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (array[i] % 2 == 0)
+         sum = sum + 1;
+   }
+   return sum;
+}
+
+Console.Clear();
+int[] array = new int[5];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.Write(ReleaseArray(array));
