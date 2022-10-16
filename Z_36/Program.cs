e96deef140d66791fb18a20d0e3b911f7d8ec004
Slava K <slava.kostromin.2000@mail.ru>
2022-10-16 17:49:21 +0700

    Решил Задачу 36

---
+++ Z_36/Program.cs
@@ -0,0 +1,23 @@
+﻿void InputArray(int[] array)
+{
+   for (int i = 0; i < array.Length; i++)
+      array[i] = new Random().Next(-10, 10);
+}
+
+
+int ReleaseArray(int[] array)
+{
+   int sum = 0;
+   for (int i = 0; i < array.Length; i++)
+   {
+      if (i % 2 == 1)
+         sum = sum + array[i];
+   }
+   return sum;
+}
+
+Console.Clear();
+int[] array = new int[4];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+Console.Write(ReleaseArray(array));
